3de3089c184d6d984a9401178a00c9e0c495bd3e
JonathanAspeling <jonathan@amberstone.digital>
2022-04-05 20:32:28 +0200

    Updated Web Route to hit GameController on Dashboard Load with Inertia JS

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\GameController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -24,8 +25,7 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [GameController::class,'index'])->middleware(['auth', 'verified'])->name('dashboard');
+Route:
 
 require __DIR__.'/auth.php';
